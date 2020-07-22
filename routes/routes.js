4d0d14930cb2ec0335d53a1d9c420757e69103c9
Thiago Penna <afonsopenna@gmail.com>
2020-07-22 10:09:22 -0300

    Preparação do ambiente

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,32 @@
-const express = require('express');
+import express from "express";
+import mongoose from "mongoose";
+
+//const { Mongoose } = require("mongoose");
 const transactionRouter = express.Router();
 
-module.exports = transactionRouter;
+transactionRouter.get("/t", (_, res) => {
+  const dadosSchema = mongoose.Schema({
+    description: { type: String },
+    value: { type: Number },
+    category: { type: String },
+    year: { type: Number },
+    month: { type: Number },
+  });
+  mongoose.model("transactions", dadosSchema);
+
+  const transactions = mongoose.model("transactions");
+
+  new transactions({
+    description: "Teste",
+  })
+    .save()
+    .then(() => {
+      console.log("Documento inserido");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.send("concluido");
+});
+
+export default transactionRouter;
